a0daa0981eb3bd71e7cceb023d423987817d61de
Angela Dyrda <adyrda2@gmail.com>
2023-08-12 19:29:30 -0500

    Add completed project

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Angela Dyrda on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
